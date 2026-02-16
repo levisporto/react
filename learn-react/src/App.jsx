259b93f3552f78c7bdf58f08a5079d3d49d6f8f0
Levi S. Porto <62207875+levisporto@users.noreply.github.com>
2026-02-16 11:58:51 -0300

    learning react from the start

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -16,17 +16,17 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Lets Learn React! :)</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Be patient, this is just the beginning of our journey. We will learn React together, step by step.
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+     AAAAAAAAAAAAAAAAAAAAAAAA
       </p>
     </>
   )
