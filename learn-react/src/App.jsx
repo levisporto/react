50b80b6f463bb7b26068717958c57d075db70da1
Levi S. Porto <62207875+levisporto@users.noreply.github.com>
2026-02-18 09:49:26 -0300

    vercel-fun

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,23 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import reactLogo from './assets/pingas.png'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const [color, setColor] = useState("red");
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Lets Learn React! :)</h1>
+      <h1>Levi React Hideout</h1>
       <div className="card">
+      <h1>My favorite color is {color}!</h1>
+      <button
+        type="button"
+        onClick={() => setColor("blue")}
+      >Blue</button>
+      <button
+        type="button"
+        onClick={() => setColor("red")}
+      >Red</button>
+      <button
+        type="button"
+        onClick={() => setColor("pink")}
+      >Pink</button>
+      <button
+        type="button"
+        onClick={() => setColor("green")}
+      >Green</button>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
